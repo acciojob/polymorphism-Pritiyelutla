918430380de93deefb17cfec4bdb284a23ba185c
Prit <pritiyelutla@gmail.com>
2024-02-15 17:21:37 +0530

    program coded

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public static void main ( String args[])
+    {
+        Product p = new Product();
 
+        int ans1 = p.product(10,10);
+        int ans2 = p.product(5,6,7);
+        double ans3 = p.product(2.5, 4.5);
+
+        System.out.println(ans1 + " " + ans2 + " " + ans3);
+    }
+    public static class Product
+    {
+        public int product(int x, int y)
+        {
+            return x * y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x * y * z;
+        }
+        public double product(double x, double y)
+        {
+            return x * y;
+        }
+    }
 }
